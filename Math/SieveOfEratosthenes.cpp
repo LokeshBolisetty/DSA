8b0102becc2e8689bbdc173095f38ceb5635e4a6
LokeshBolisetty <lokesh.bolisetty@gmail.com>
2021-07-24 14:15:09 +0530

    Sieve

--- Math/SieveOfEratosthenes.cpp
+++ Math/SieveOfEratosthenes.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 vector<int> SieveOfEratosthenes(int A){
-    vector<bool> b(A,true);
+    vector<bool> b(A+1,true);
     for(int i=2;i*i<=A;i++){
-        for(int j=i*i;j<A;j=j+i){
+        for(int j=i*i;j<=A;j=j+i){
             b[j]=false;
         }
     }
     vector<int> prime;
-    for(int i=2;i<A;i++){
+    for(int i=2;i<=A;i++){
 
         if(b[i])prime.push_back(i);
     }

9e2c0d8b0b85aa724f873ae9a41120df1229f164
LokeshBolisetty <lokesh.bolisetty@gmail.com>
2021-08-02 19:30:31 +0530

    temp

--- Graphs/temp.cpp
+++ Graphs/temp.cpp
@@ -1,61 +1,47 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-//THIS IS FOR A DIRECTED GRAPH. UNDIRECTED GRAPHS HAVE TO BE TAKEN CARE OF.
-
-
-void BellmanFord(vector<vector<int>> edges,int v,int e,int start){
-    vector<int> distance(v+1,INT_MAX);
-    distance[start] = 0;
-    bool flag = true;
-    for(int i = 0;i<v-1;i++){
-        int count = 0;
-        for(int j = 0;j<e;j++){
-            int source = edges[j][0];
-            if(distance[source]==INT_MAX)continue;
-            int dest = edges[j][1];
-            int weight = edges[j][2];
-            if(distance[dest]>distance[source]+weight){
-                count++;
-                distance[dest] = distance[source]+weight;
+//THIS IS FOR A DIRECTED GRAPH. UNDIREECTED GRAPHS HAVE TO BE TAKEN CARE OF
+vector<int> dijkstra(int v,vector<pair<int,int>> adj[],int start){
+    vector<bool> known(v+1,false);
+    vector<int> cost(v+1,INT_MAX);
+    vector<int> path(v+1,-1); 
+    priority_queue<pair<int,int>> pq;
+    cost[start] = 0;
+    pq.push(make_pair(0,start));
+    while(!pq.empty()){
+        pair<int,int> p = pq.top();
+        pq.pop();
+        if(known[p.second])continue;
+        known[p.second] = true;
+        int currentCost = -p.first;
+        for(int i =0;i<adj[p.second].size();i++){
+            int weight = adj[p.second][i].second;
+            int neighbour = adj[p.second][i].first;
+            if(!known[neighbour]){
+                if(cost[neighbour]>currentCost+weight){
+                    cost[neighbour] = currentCost+weight;
+                    path[neighbour] = p.second;
+                    pq.push(make_pair(-cost[neighbour],neighbour));
+                }
             }
         }
-        if(count == 0){
-            flag = false;
-            break;
-        }
     }
-    if(flag){
-        for(int j = 0;j<e;j++){
-                int source = edges[j][0];
-                if(distance[source]==INT_MAX)continue;
-                int dest = edges[j][1];
-                int weight = edges[j][2];
-                if(distance[dest]>distance[source]+weight){
-                    printf("Graph consists of a negative cycle :(\n");
-                    return;
-                }
-        }
-    }
-    for(int i =1;i<v+1;i++){
-        cout<<i<<"\t"<<distance[i]<<endl;
-    }
+    return path;
 }
+
 int main(){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int v,e;
-    cin>>v>>e;
+    int v,e,start;
+    cin>>v>>start>>e;
     vector<pair<int,int>> adj[v+1];
-    vector<vector<int>> edges;
-    for(int i =0;i<e;i++){
+    for(int i=0;i<e;i++){
         int a,b,c;
         cin>>a>>b>>c;
         adj[a].push_back(make_pair(b,c));
-        edges.push_back({a,b,c});
+    } 
+    vector<int> path = dijkstra(v,adj,start);
+    for(int i =0;i<v+1;i++){
+        cout<<i<<"\t"<<path[i]<<endl;
     }
-    
-    BellmanFord(edges,v,e,1);
     return 0;
 }
